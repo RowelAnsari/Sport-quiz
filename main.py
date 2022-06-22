60e038c3ea5a474229425148b8c4e793b67b2f72
Rowel Ansari <>
2022-06-22 00:13:08 +0000

    adding questions

--- main.py
+++ main.py
@@ -3,14 +3,8 @@ import random
 from PIL import Image, ImageTk
 
 names = []
-global questions_answers
 asked = []
-
-questions_answers = {
-    1: ["Who won the 2002 FIFA World cup?", 'Brazil', 'America','UK', 'Austrailia' ,'Brazil',1],
-    2: ["Which NBA player has won the most championships?",'Michael Jordan','Bill Russel','Lebron James', 'Wilt Chamberlain','Bill Russel',2],
-    3: ["Which position in soccer is allowed to touch the ball?", 'Forward','Midfielder', 'Defender','Goalkeeper','Goalkeeper',4],
-}
+score=0
 
 def randomiser():
     global qnum
@@ -26,22 +20,24 @@ class QuizStarter:
     background_color="lightgrey"
 
     self.heading_label=Label(window, text = "Sports quiz", font =( "Tw Cen MT","18","bold"),bg=background_color)
-    self.heading_label.grid(row= 0, padx=20)
+    self.heading_label.place(x=100,y=100)
 
     self.var1=IntVar()
 
-    self.user_label=Label(window, text="Please Enter your Username Below: ", font=( "Tw Cen MT","18","bold"),bg=background_color)
-    self.user_label.grid(row=1, padx=20, pady=20)
+    self.user_label=Label(window, text="Enter name: ", font=( "Tw Cen MT","18","bold"),bg=background_color)
+    self.user_label.place(x=468,y=100)
 
     self.entry_box=Entry(window)
-    self.entry_box.grid(row=2,padx=20, pady=20)
+    self.entry_box.place(x=475,y=150)
 
-    self.continue_button = Button(window, text="Continue", font=( "Helvetica","13","bold"), bg="darkgrey",command=self.name_collection)
-    self.continue_button.grid(row=3,padx=20, pady=20)
+    self.continue_button = Button(window, text="Start Quiz", font=( "Helvetica","13","bold"), bg="lightblue",command=self.name_collection)
+    self.continue_button.place(x=500, y=200)
+    
 
   def name_collection(self):
         name=self.entry_box.get()
         names.append(name)
+        print(names)
         self.heading_label.destroy()
         self.user_label.destroy()
         self.entry_box.destroy()
@@ -50,8 +46,17 @@ class QuizStarter:
 
 class Quiz:
 
-   def __init__(self, parent):
+  def __init__(self, parent):
     background_color="lightgrey"
+    
+    self.qa_dictionary = {
+    1: ["Who won the 2002 FIFA World cup?", 'Brazil', 'America','UK', 'Austrailia' ,'Brazil',1],
+    2: ["Which NBA player has won the most championships?",'Michael Jordan','Bill Russel','Lebron James', 'Wilt Chamberlain','Bill Russel',2],
+    3: ["Which position in soccer is allowed to touch the ball?", 'Forward','Midfielder', 'Defender','Goalkeeper','Goalkeeper',4],
+    4:["How many gold medals has Usain Bolt won?",'6','8','9','7','8',2,], 
+    5:["What "]
+
+}
   
   
     self.quiz_frame = Frame(parent, bg = background_color, padx=40, pady=40)
@@ -59,61 +64,65 @@ class Quiz:
 
     randomiser()
 
-    self.question_label=Label(window, text = questions_answers[qnum][0], font =( "Tw Cen MT","18","bold"))
+    self.question_label=Label(window, text = self.qa_dictionary[qnum][0], font =( "Tw Cen MT","18","bold"))
     self.question_label.grid(row= 0, padx=10, pady=10)  
 
     self.var1=IntVar()
 
-    self.rb1 = Radiobutton(window, text = questions_answers[qnum][1], font=("Helvetica", "12"), bg=background_color, value=1, variable=self.var1, pady=10)
+    self.rb1 = Radiobutton(window, text = self.qa_dictionary[qnum][1], font=("Helvetica", "12"), bg=background_color, value=1, variable=self.var1, pady=10)
     self.rb1.grid(row=1, sticky=W)
 
-    self.rb2 = Radiobutton(window, text = questions_answers[qnum][2], font=("Helvetica", "12"), bg=background_color, value=2, variable=self.var1, pady=10)
+    self.rb2 = Radiobutton(window, text = self.qa_dictionary[qnum][2], font=("Helvetica", "12"), bg=background_color, value=2, variable=self.var1, pady=10)
     self.rb2.grid(row=2, sticky=W)
 
-    self.rb3 = Radiobutton(window, text = questions_answers[qnum][3], font=("Helvetica", "12"), bg=background_color, value=3, variable=self.var1, pady=10)
+    self.rb3 = Radiobutton(window, text = self.qa_dictionary[qnum][3], font=("Helvetica", "12"), bg=background_color, value=3, variable=self.var1, pady=10)
     self.rb3.grid(row=3, sticky=W)
 
-    self.rb4 = Radiobutton(window, text = questions_answers[qnum][4], font=("Helvetica", "12"), bg=background_color, value=4, variable=self.var1, pady=10)
+    self.rb4 = Radiobutton(window, text = self.qa_dictionary[qnum][4], font=("Helvetica", "12"), bg=background_color, value=4, variable=self.var1, pady=10)
     self.rb4.grid(row=4, sticky=W)
 
     self.confirm_button = Button(window, text="Confrim",bg="white",command=self.test_progress)
-    self.confirm_button.grid(row=6)
+    self.confirm_button.place(x=300,y=235)
     self.score_label  = Label(window, text = 
                              'score')
-    self.score_label.grid(row= 7)  
+    self.score_label.place(x=390,y=240)  
+
+    self.quit=Button(window,text="Quit",font=("Helvetica","13","bold"),bg="red2",command=self.end_screen)
+    self.quit.place(x=50,y=235)
      
      
-   def questions_setup(self):
+  def questions_setup(self):
      randomiser()
      self.var1.set(0)
-     self.question_label.config(text=questions_answers[qnum][0])
-     self.rb1.config(text=questions_answers[qnum][1])
-     self.rb2.config(text=questions_answers[qnum][2])
-     self.rb3.config(text=questions_answers[qnum][3])
-     self.rb4.config(text=questions_answers[qnum][4]) 
-
+     self.question_label.config(text=self.qa_dictionary[qnum][0])
+     self.rb1.config(text=self.qa_dictionary[qnum][1])
+     self.rb2.config(text=self.qa_dictionary[qnum][2])
+     self.rb3.config(text=self.qa_dictionary[qnum][3])
+     self.rb4.config(text=self.qa_dictionary[qnum][4]) 
   
-   def test_progress(self):
+  def test_progress(self):
       global score 
-      score = 0
       scr_label=self.score_label 
       choice=self.var1.get()
-      if len(asked)>9:
-        if choice == questions_answers[qnum][6]:
+      if len(asked)>2:
+        if choice == self.qa_dictionary[qnum][6]:
           score +=1 
           scr_label.configure(text=score) 
           self.confirm_button.config(text="Confirm")
+          self.end_screen()
+          
         else:
           score+=0 
-          scr_label.configure(text="The correct answer was: "+ questions_answers[qnum][5] ) 
+          scr_label.configure(text="The correct answer was: "+ self.qa_dictionary[qnum][5] ) 
           self.confirm_button.config(text="confirm")
+          self.end_screen()
       
       else:
             if choice==0: 
               self.confirm_button.config(text="Try Again, you didn't select an option then submit again" )
               choice=self.var1.get() 
             else:
-              if choice == questions_answers[qnum][6]: 
+              if choice == self.qa_dictionary[qnum][6]: 
                 score+=1
                 scr_label.configure(text=score)
                 self.confirm_button.config(text="confirm")
@@ -121,21 +130,78 @@ class Quiz:
   
               else:
                   score+=0
-                  scr_label.configure(text="The correct answer was: " + questions_answers[qnum][5])
+                  scr_label.configure(text="The correct answer was: " + self.qa_dictionary[qnum][5])
                   self.confirm_button.config(text="Confirmn")
                   self.questions_setup()
-       
+    
+  def end_screen(self):
+    window.withdraw()
+    end_object=End()
 
+class End:
+  def _innit_(self):
+    background="OldLace"
+    self.end_box= Toplevel(window)
+    self.end_box.title("End Box")
 
+    self.end_frame = Frame (self.end_box,width=1000,height=1000,bg=background)
+    self.end_frame.grid(row=1)
+
+    end_heading = Label(self.end_frame,text='Well Done',font=('Tw Cen Mt',22,'bold'),bg=background,pady=15)
+    end_heading.grid(row=0)
+
+    exit_button=Button (self.end_frame,text='Exit',width=10,bg="indianRed1",font=('Tw Cen Mt',12,'bold'),command=self.close_end)
+    exit_button.grid(row=4,pady=20)
+
+    self.listLabel = Label(self.end_frame,text="1st Place Available",font=('Tw Cen MT'),width=40,bg=backround,padx=10,pady=10)
+    self.listLabel.grid(column=0,row=2)
+  
+  
+  def close_end(self):
+    self.end_box.destroy()
+    window.withdraw()
+  
+  def end_screen(self):
+    window.withdraw()
+    name=names[0]
+    file=open("leaderBoard.txt","a")
+    
+    if name== "admin_reset":
+      file=open("leaderboard.txt","w")
+    else:
+      file.write(str(score))
+      file.write(" - ")
+      file.write(name+"\n")
+      file.close
+  
+    inputFile = open("leaderBoard.txt","r")
+    lineList = inputFile.readlines()
+    lineList.sort()
+    top=[]
+    top5=(lineList[-5:])
+    for line in top5:
+      point=line.split(" - ")
+      top.append((int(point[0]),point[1]))
+    file.close()
+    top.sort()
+    top.reverse()
+    return_string = ""
+    for i in range(len(top)):
+      return_string+="{} - {}\n".format(top[i][0],top[i][1])
+    print(return_string)
+  
+    end_object=End()
+    end_object.listLabel.config(text=return_string)
+    
 if __name__== "__main__":
     window = Tk()
     window.title("12CSC Quiz")
-    window.geometry("600x600")
-    bg_image = Image.open("sports.jpg")
+    window.geometry("700x600")
+    bg_image = Image.open("guy running.png")
     bg_image = bg_image.resize((1000,600),Image.ANTIALIAS)
     bg_image = ImageTk.PhotoImage(bg_image)
     image_label= Label(window, image=bg_image)
     image_label.place(x=0, y=0, relwidth=1, relheight=1)
     start_object = QuizStarter(window)
-
+    
     window.mainloop()
